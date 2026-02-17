8aeacacf22962884153f6a6dd4ca525f00b322b2
Mrigaank Sharma <mrigaanksharma928@gmail.com>
2026-02-17 20:57:32 +0530

    done final

--- components/Header.tsx
+++ components/Header.tsx
@@ -63,7 +63,7 @@ function Header() {
 
         <motion.a
         initial={{y:30,opacity:0}} whileInView={{y:0,opacity:1}} transition={{duration:0.6,delay:1}}
-          href="/Mrigaank Resume 2025 2.pdf"
+          href="/mrigaank resume.pdf"
           download
           className="
             px-10 py-3 
